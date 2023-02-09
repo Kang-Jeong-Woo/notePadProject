3044626f5e375be09f16a555bd6f0bd9d163f492
강정우 <zzangkbc1@gmail.com>
2023-02-09 15:37:23 +0900

    [업데이트] dragalbe 구현 완, 호버링 시 삭제, 고정 표시 => log event까지

--- src/components/UI/PostIt.js
+++ src/components/UI/PostIt.js
@@ -1,23 +1,61 @@
 import classes from "./PostIt.module.css";
-import {useRef} from "react";
+import {Fragment, useRef, useState} from "react";
+import {Transition} from "react-transition-group";
+
 
 const PostIt = props => {
-    const postionRef = useRef();
-    const moveTrigger = (event) => {
-        postionRef.current.style.position = "absolute";
-        document.body.append(postionRef);
-        function moveAt(pageX,pageY) {
-            postionRef.current.style.left = pageX - postionRef.offsetWidth / 2 + "px";
-            postionRef.current.style.top = pageY - postionRef.offsetHeight / 2 + "px";
-        }
+    const postionRef = useRef(undefined);
+    const tabRef = useRef(undefined);
 
-        console.log(postionRef.offsetHeight);
-        moveAt(event.pageX, event.pageY);
+    let shiftX;
+    let shiftY;
+
+    function moveAt(pageX, pageY) {
+        postionRef.current.style.left = pageX - shiftX + "px";
+        postionRef.current.style.top = pageY - shiftY + "px";
     }
 
+    function onMouseMove(event) {
+        moveAt(event.pageX, event.pageY)
+    }
 
-    return(
-        <div onMouseDown={moveTrigger} className={classes.postIt} ref={postionRef}>{props.content}</div>
-    )
+    const moveTrigger = (event) => {
+        shiftX = event.clientX-postionRef.current.getBoundingClientRect().left;
+        shiftY = event.clientY-postionRef.current.getBoundingClientRect().top;
+        postionRef.current.style.position = "absolute";
+        moveAt(event.pageX, event.pageY);
+        document.addEventListener("mousemove", onMouseMove);
+    }
+
+    const stopTrigger = (event) => {
+        document.removeEventListener("mousemove", onMouseMove);
+    }
+
+    const mouseIn = (event) => {
+        tabRef.current.style.top = "5px"
+    }
+    const mouseOut = (event) => {
+        tabRef.current.style.top = "-20px"
+    }
+
+    const pinEvent = () => {
+        console.log("고정!");
+    }
+    const closeEvent = () => {
+        console.log("삭제!");
+    }
+
+    return (
+        <div onMouseDown={moveTrigger} onMouseUp={stopTrigger} onMouseEnter={mouseIn} onMouseLeave={mouseOut}
+             ref={postionRef} className={classes.postIt}>
+            <div style={{position: "relative", height:"0px"}}>
+            <span className={classes.tab} ref={tabRef}>
+                <span onClick={pinEvent}>pin</span>
+                <span onClick={closeEvent}>X</span>
+            </span>
+                <div className={classes.content}>{props.content}</div>
+            </div>
+        </div>
+    );
 }
 export default PostIt;
