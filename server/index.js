2d48a844a4d88b2cc7d9ba596c98c527cf8ec81f
강정우 <zzangkbc1@gmail.com>
2023-02-28 00:57:48 +0900

    [업데이트] 서버 번호 수정

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({extended: true}));
 app.use(cors({
-    origin : "https://da-g-gu.vercel.app",
+    origin : "http://localhost:3000",
     methods : ['GET', 'POST'],
     credentials : true
 }))
